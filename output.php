cc801453722cf5856c95a1dc9bddbbb1c6683311
Mikolaj Florysiak <32200620+mikkmjf@users.noreply.github.com>
2021-11-20 18:52:29 +0000

    Add files via upload

---
+++ output.php
@@ -0,0 +1,8 @@
+<!-- run config file -->
+<?php include "config.php"; ?>
+
+<!-- output specific translated part  -->
+  <title><?php echo $lang['PAGE_TITLE']; ?></title>
+
+<!-- setup translation switch -->
+<a href="?lang=en"><img src="img/ukflag.png" width="20" height="20" alt="English"></a>  <a href="?lang=pl"><img src="img/plflag.png" width="20" height="20" alt="Polski"></a>
