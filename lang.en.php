86a627805d448d1053bcfa95fbe3026dda6b7598
Mikolaj Florysiak <32200620+mikkmjf@users.noreply.github.com>
2021-11-20 18:55:00 +0000

    Add files via upload

---
+++ lang.en.php
@@ -0,0 +1,13 @@
+<?php
+/* 
+------------------
+Language: English
+------------------
+*/
+
+$lang = array();
+
+// setup variable name with name of section = translated text
+
+$lang['PAGE_TITLE'] = 'DriveWell - Driving Instructor Weston-super-Mare, Portishead, Clevedon';
+?>
---
+++ lang.pl.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+-----------------
+Language: Polish
+-----------------
+*/
+
+$lang = array();
+
+// setup variable name with name of section = translated text
+
+$lang['PAGE_TITLE'] = 'DriveWell - Nauka Jazdy Weston-super-Mare, Portishead, Clevedon';
+
+?>
